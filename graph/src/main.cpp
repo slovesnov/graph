61ed07c60895274cf66a08346d7beb122765bff5
aleksey slovesnov <slovesnov@yandex.ru>
2022-05-07 06:01:27 +0300

    1

--- graph/src/main.cpp
+++ graph/src/main.cpp
@@ -11,14 +11,6 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-//	try {
-//		ExpressionEstimator e;
-//		e.calculate("tan(x0)");
-//		std::cout << v;
-//	} catch (Exception &e) {
-//		std::cout << e.what() << std::endl;
-//	}
-
 	gtk_init (&argc, &argv);
 	aslovInit(argv);
 	GraphWindow c;
